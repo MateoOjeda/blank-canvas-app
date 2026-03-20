eb16c8f2ae792db4a72281e47901544b96539463
MateoOjeda <mateo_ojeda_lado@hotmail.com>
2026-03-19 23:30:20 -0300

    vamos a ver si se cambio

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,5 @@
 const Index = () => {
-<<<<<<< HEAD
-  return (
-    <div className="min-h-screen bg-background text-foreground">
-    </div>
-  );
-=======
   return <div className="min-h-screen bg-background" />;
->>>>>>> viejo/main
 };
 
 export default Index;
